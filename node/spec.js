63ff5292b1fe7aed93d1f14e1982f8835b00f668
Jessie Morris <jmorris2@pillartechnology.com>
2018-09-04 18:46:57 -0400

    Crappy multi-stage docker

--- node/spec.js
+++ node/spec.js
@@ -3,6 +3,6 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 var Index = require('./index');
 describe('Index', function() {
   it('passes', function() {
-    expect(Index(5)).to.equal("buzz");
+    expect(Index(5)).to.equal("buz");
   });
 });
